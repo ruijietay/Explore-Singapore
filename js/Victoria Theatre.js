c0b268363bcc8421c5f4898737a8bc4af35b8bb5
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-19 21:44:38 +0800

    Done with Victoria Theatre (Base)

--- js/Victoria Theatre.js
+++ js/Victoria Theatre.js
@@ -1,4 +1,5 @@
-// JavaScript source code
+
+//Google Maps
 function initMap() {
     var victoriaTheatre = { lat: 1.2883375, lng: 103.85181260000002 };
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -9,4 +10,19 @@ function initMap() {
         position: victoriaTheatre,
         map: map
     });
+    // Info Window Content
+    var infoWindowContent =
+        '<div class="info_content">' +
+            '<h3>Victoria Theatre & Concert Hall</h3>' +
+            '<p>Home to the Singapore Symphony Orchestra, the refurbished Victorian building features a clock tower.</p>' + '</div>';
+
+    var infoWindow = new google.maps.InfoWindow(), marker;
+
+    // Allow each marker to have an info window    
+    google.maps.event.addListener(marker, 'click', (function (marker) {
+        return function () {
+            infoWindow.setContent(infoWindowContent);
+            infoWindow.open(map, marker);
+        }
+    })(marker));
 }
