9985f328747b0cb2e413be60317cc925c6c8aabf
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-20 21:02:17 +0800

    Esplanade Done

--- js/Esplanade.js
+++ js/Esplanade.js
@@ -2,20 +2,20 @@
 
 //Google Maps
 function initMap() {
-    var NationalGallery = { lat: 1.289704, lng: 103.85128499999996 };
+    var Esplanade = { lat: 1.2897934, lng: 103.85581660000003 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: NationalGallery
+        center: Esplanade
     });
     var marker = new google.maps.Marker({
-        position: NationalGallery,
+        position: Esplanade,
         map: map
     });
     // Info Window Content
     var infoWindowContent =
         '<div class="info_content">' +
-        '<h3>National Gallery Singapore</h3>' +
-        '<p>Museum of Singapore & Southeast Asian arts housed in a restored municipal building dating to 1929.</p>' + '</div>';
+        '<h3>Esplanade</h3>' +
+        '<p>Futuristic waterfront arts venue with 1,600-seat concert hall, 2,000-person theatre, studios & mall.</p>' + '</div>';
 
     var infoWindow = new google.maps.InfoWindow(), marker;
 
@@ -26,4 +26,33 @@ function initMap() {
             infoWindow.open(map, marker);
         }
     })(marker));
+}
+
+//Facilities
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+//Animation for accordion
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
