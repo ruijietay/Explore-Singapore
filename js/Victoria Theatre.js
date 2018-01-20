990672cffee77b2363c51a3d2b3db29f970f9c4b
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-20 18:08:30 +0800

    accodion animation

--- js/Victoria Theatre.js
+++ js/Victoria Theatre.js
@@ -42,3 +42,16 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+//Animation for accordion
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
