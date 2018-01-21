b05218fadcfadabab1dc4fb9904d15ed2f66f5ce
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-22 03:39:15 +0800

    Final Push

--- js/Victoria Theatre.js
+++ js/Victoria Theatre.js
@@ -55,3 +55,22 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+/*signupform*/
+function haveoverlay1() {
+    document.getElementById("overlay1").style.display = "block";
+}
+
+function turnoffoverlay1() {
+    document.getElementById("overlay1").style.display = "none";
+} /*element with id "overlay" will have a style of att display set to none; (display:none;)*/
+
+function myPassword() {
+    var pass = document.getElementById("passwordsignup");
+    if (pass.type === "password") { /*if the type of input is password, then change the type to text if on click*/
+        pass.type = "text";
+    }
+    else {
+        pass.type = "password" /*else if the type is not password (text), change back to password. This happens when checkbox is unclicked.*/
+    }
+}
