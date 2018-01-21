6191094ded08d24442863305c5bb7e8567746ce6
Musfirah <Musfirah@LAPTOP-C85KLE6R>
2018-01-22 00:11:11 +0800

    I pushed

--- js/thrill-seekers.js
+++ js/thrill-seekers.js
@@ -264,4 +264,21 @@ function showRoute3() {
     popup.classList.toggle("show");
 }/*popup for location uss suggested route*/
 /*-------------------------------------------------------------------------------------------------------------------------------------*/
+/*signupform*/
+function haveoverlay1() {
+    document.getElementById("overlay1").style.display = "block";
+}
 
+function turnoffoverlay1() {
+    document.getElementById("overlay1").style.display = "none";
+} /*element with id "overlay" will have a style of att display set to none; (display:none;)*/
+
+function myPassword() {
+    var pass = document.getElementById("passwordsignup");
+    if (pass.type === "password") { /*if the type of input is password, then change the type to text if on click*/
+        pass.type = "text";
+    }
+    else {
+        pass.type="password" /*else if the type is not password (text), change back to password. This happens when checkbox is unclicked.*/
+    }
+}
